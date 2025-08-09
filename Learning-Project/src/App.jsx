5d6d9a03e42d406fd20afd1abafe7b2d07914aa0
justbappy <yennamvishal1883@gmail.com>
2025-08-09 20:38:05 +0530

    React Basics

--- Learning-Project/src/App.jsx
+++ Learning-Project/src/App.jsx
@@ -4,9 +4,28 @@ function App() {
 
   return (
     <>
-      <h1>Welcome World</h1>
+      <NetflixSeries/>
+      <NetflixSeries/>
+      <NetflixSeries/>
     </>
   )
 }
 
 export default App
+
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img src="qot.webp" alt="" />
+      </div>
+      <div>
+        <h1>Name: Queen of Tears</h1>
+        <h3>Rating: 8.2</h3>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias obcaecati, dolorem enim maiores velit incidunt nemo similique non quae numquam cupiditate labore fugit quia explicabo amet tenetur totam in optio?</p>
+      </div>
+    </div>
+  )
+}
